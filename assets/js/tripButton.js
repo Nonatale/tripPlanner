7a2489297177c7d0411cd260df9072d9b7e99e9d
Ryan Fredericks <Kurt@stephanies-mbp.myfiosgateway.com>
2024-08-19 20:38:42 -0400

    Updating backgrounds

--- assets/js/tripButton.js
+++ assets/js/tripButton.js
@@ -45,6 +45,31 @@ function tripFormSubmit(event) {
     }
     
 }
+//Have folder on images, function on line 74 would create a randomly generated number to grab image.
+//Creates an array of images
+const imageArray = [
+    'https://media.9news.com/assets/KUSA/images/7f6e78a1-dd06-48e2-85b2-6e35a08d6832/7f6e78a1-dd06-48e2-85b2-6e35a08d6832_1140x641.jpg',
+    'https://cdn.aarp.net/content/dam/aarp/travel/Domestic/2021/12/1140-oahu-hero.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT78NCXBn45EBPyNn3zfSjQe9kCxwiddFkH2A&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR1D45iBVsNs0SCKLbS2NwDW_2elEzF74vsQ&s'
+
+];
+//Grabs random image from image array
+const randomImage = Math.floor(math.random() * imageArray.length);
+
+//Creates a new image element
+const newImage = document.createElement('img');
+newImage.src = imageArray[randomIndex];
+newImage.alt = 'randomImage';
+
+//Appened the new image to container
+const container = document.getElementById('imageContainer');
+container.appendChild(newImage);
+
+//Adds randomly generated background image to trip created by user 
+function displayBackgroundImage(many){
+    var arr = randomImages
+} 
 
 // Adds html elements of the trip to the page and give it a background img
 function displayTrip() {
