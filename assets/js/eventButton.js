e7b719e51fddbdc59bc8be62d26a60d9c86366f6
shukikat <kathuriashuki@gmail.com>
2024-08-20 22:26:45 -0400

    made a few updates to render events to display

--- assets/js/eventButton.js
+++ assets/js/eventButton.js
@@ -9,6 +9,7 @@ const place = document.querySelector("#place");
 const note = document.querySelector("#note");
 const price = document.querySelector("#price");
 const reservation = document.querySelector("#reservation");
+const eventBox = document.querySelector("#eventBox");
 
 const activityForm = document.querySelector("#activityForm");
 
@@ -32,21 +33,14 @@ function activityFormSubmit(event) {
     // Proceeds if required data is not empty. Otherwise, prints error message
     if (ifEmptyData(title, date, time, place)) {
 
-        const activity = {
-            title: title.value.trim(),
-            date: date.value.trim(),
-            time: time.value.trim(),
-            place: place.value.trim(),
-            note: note.value ? note.value.trim() : undefined,
-            trip: localStorage.getItem("currTrip")
-        }
-
+       
         updateActivityList(activity);
 
         activityForm.classList.add("hidden");
     } else {
         activityFormError();
     }
+
 }
 
 // Adds new activity to a trip and then sorts the events and trips
@@ -72,7 +66,7 @@ function getTripIndex(tripName) {
 // Sort the trips based on the date and time of the first activity
 function sortTrips() {
     const tripList = JSON.parse(localStorage.getItem("tripList"));
-    
+
     tripList.sort((trip1, trip2) => {
         // Ensure both trips have at least one activity with a valid date and time
         if (trip1.activity.length > 0 && trip2.activity.length > 0) {
@@ -86,7 +80,7 @@ function sortTrips() {
         // If one or both trips have no activities, consider them equal in sorting
         return 0;
     });
-    
+
     localStorage.setItem("tripList", JSON.stringify(tripList));
 }
 
@@ -111,32 +105,80 @@ function activityFormError() {
     activityForm.appendChild(errorMsg);
 }
 
+const activity = {
+    title: title.value.trim(),
+    date: date.value.trim(),
+    time: time.value.trim(),
+    place: place.value.trim(),
+    note: note.value ? note.value.trim() : undefined,
+    trip: localStorage.getItem("currTrip")
+}
+
+
 function displayActivities() {
     const tripList = JSON.parse(localStorage.getItem("tripList"));
     const tripIndex = getTripIndex(localStorage.getItem("currTrip"));
     const trip = tripList[tripIndex];
     // Display Logic
-    let currDate;
+    // let currDate;
     let activityIndex = 0;
     // Loops through 
-    while (trip.activity[activityIndex]) {
-        if (!currDate || currDate !== activity.date) {
+    eventBox.innerHTML = "";
+    // while (trip.activity[activityIndex]) {
+    //     if (!currDate || currDate !== activity.date) {
             const dayBox = document.createElement("section");
             const dateBox = document.createElement("div");
+            const dayAndDate=document.createElement("h4");
             const infoBox = document.createElement("div");
+            const timeBox=document.createElement("time-box")
+            const time=document.createElement("h4");
+            const iconBox=document.createElement("div");
+            const activitySquare=document.createElement("div");
+            //do i need to add for activity square input type, etc?
+            const activityBox=document.createElement("div");
+            const titleBox=document.createElement("div");
+            const event=document.createElement("h4");
+            const location=document.createElement("h5");
+            const noteBox=document.createElement("div");
+            const deleteBox=document.createElement("div");
             dayBox.classList.add("day-box");
             dateBox.classList.add("date-box");
             infoBox.classList.add("info-box");
+            timeBox.classList.add("time-box");
+            iconBox.classList.add("icon-box");
+            activitySquare.classList.add("activity-square");
+            activityBox.classList.add("activity-box");
+            titleBox.classList.add("title-box");
+            noteBox.classList.add("note-box");
+            deleateBox.classList.add("delete-box");
             eventBox.appendChild(dayBox);
             dayBox.appendChild(dateBox);
+            dateBox.appendChild(dayAndDate); 
             dayBox.appendChild(infoBox);
-        }
+            infoBox.appendChild(timeBox);
+            timeBox.appendChild(time);
+            infoBox.appendChild(iconBox);
+            iconBox.appendChild(activitySquare);
+            infoBox.appendChild(activityBox);
+            activityBox.appendChild(titleBox);
+            titleBox.appendChild(event);
+            titleBox.appendChild(location);
+            activityBox.appendChild(noteBox);
+            infoBox.appendChild(deleteBox);
+
+
+
+
+            
+    
         activityIndex++;
     }
 
 
-}
+
 
 activityButton.addEventListener("click", activityFormClick);
 submitButton.addEventListener("click", activityFormSubmit);
 document.addEventListener("DOMContentLoaded", displayActivities);
+
+
